b36be35a1897dd119e8266468a5eb9d96ac84d46
Maayan Abramov <maayan312a@gmail.com>
2025-12-06 20:48:55 +0200

    hw2 first iteration

--- assignments/assignments02/properties.sv
+++ assignments/assignments02/properties.sv
@@ -11,6 +11,21 @@ module properties #(parameter FLOORS = 5)
     input                    DoorsOp doorsOp,
     input                    EngineOp engineOp
 );
+ logic [FLOORS-1:0] floor_on_first_cycle;
+ always @(posedge clk) 
+    begin
+        if ($rose(rst)) begin
+            floor_on_first_cycle <= currentFloor;
+    end
+ end
+
+sequence sQ7;
+    !(direction == DOWN) throughout (( engineOp == STOP && doorsOp == OPEN && $changed(currentFloor) )[->FLOORS]);
+endsequence
+
+sequence sQ8;
+    !(direction == UP) throughout (( engineOp == STOP && doorsOp == OPEN && $changed(currentFloor) )[->FLOORS]);
+endsequence
 
 // ASSUME 1: Assume elevator moves up if engineOp is UP.
 property prop_1;
@@ -26,62 +41,62 @@ assume_2: assume property (prop_2);
 
 // QUESTION 1(a): Assume elevator doesn't move if engineOp is STOP.
 property prop_Q1a;
-    @(posedge clk) 1; // EDIT THIS LINE
+    @(posedge clk) (engineOp == STOP) |=> $stable(currentFloor) ;
 endproperty
 assume_Q1a: assume property (prop_Q1a);
 
 // QUESTION 1(b): Assume we start from some (specific, single) floor.
 // NOTE: You are required to use auxiliary code for this question.
 property prop_Q1b;
-    @(posedge clk) 1; // EDIT THIS LINE
+    @(posedge clk) $onehot(floor_on_first_cycle); // EDIT THIS LINE
 endproperty
 assume_Q1b: assume property (prop_Q1b);
 
 // QUESTION 2: Check we don't hit the basement.
 property prop_Q2;
-    @(posedge clk) 1; // EDIT THIS LINE
+    @(posedge clk) (currentFloor[0] == 1) |-> !(direction == DOWN) ; // EDIT THIS LINE
 endproperty
 assert_Q2: assert property (prop_Q2);
 
 // QUESTION 3: Check we don't hit the roof.
 property prop_Q3;
-    @(posedge clk) 1; // EDIT THIS LINE
+    @(posedge clk) (currentFloor[FLOORS-1] == 1) |-> !(direction == UP); // EDIT THIS LINE
 endproperty
 assert_Q3: assert property (prop_Q3);
 
 // QUESTION 4: Check door safety.
 property prop_Q4;
-    @(posedge clk) 1; // EDIT THIS LINE
+    @(posedge clk) (doorsOp == OPEN) |-> !(engineOp = GO); // EDIT THIS LINE
 endproperty
 assert_Q4: assert property (prop_Q4);
 
 // QUESTION 5:
 property prop_Q5;
-    @(posedge clk) 1; // EDIT THIS LINE
+    @(posedge clk) (requestFloor[0] == 1) |-> s_eventually (currentFloor[0] == 1 && engineOp == STOP && doorsOp == OPEN); // EDIT THIS LINE
 endproperty
 assert_Q5: assert property (prop_Q5);
 
 // QUESTION 6:
 property prop_Q6;
-    @(posedge clk) 1; // EDIT THIS LINE
+    @(posedge clk) (requestFloor[0] == 1) ##0 (!(currentFloor[0] == 1))[*40] ##1 (currentFloor[0] == 1) ; // EDIT THIS LINE
 endproperty
 cover_Q6: cover property (prop_Q6);
 
 // QUESTION 7:
 property prop_Q7;
-    @(posedge clk) 1; // EDIT THIS LINE
+    @(posedge clk)  sQ7;  // EDIT THIS LINE
 endproperty
 cover_Q7: cover property (prop_Q7);
 
 // QUESTION 8:
 property prop_Q8;
-    @(posedge clk) 1; // EDIT THIS LINE
+    @(posedge clk) sQ8; // EDIT THIS LINE
 endproperty
 cover_Q8: cover property (prop_Q8);
 
 // QUESTION 9:
 property prop_Q9;
-    @(posedge clk) 1; // EDIT THIS LINE
+    @(posedge clk) (sQ7 ##1 sQ8)[*10]; // EDIT THIS LINE
 endproperty
 cover_Q9: cover property (prop_Q9);
 
