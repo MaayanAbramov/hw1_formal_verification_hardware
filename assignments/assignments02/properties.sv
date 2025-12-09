d51623087d8d9699bfc5b7df2e056dbd3598c566
Maayan Abramov <maayan312a@gmail.com>
2025-12-09 19:36:16 +0200

    .

--- assignments/assignments02/properties.sv
+++ assignments/assignments02/properties.sv
@@ -27,7 +27,7 @@ end
 
 
 sequence sQ7;
-    1 ##1 (direction == UP) throughout (( engineOp == STOP && doorsOp == OPEN && currentFloor == $past(currentFloor << 1) ##1 ( engineOp == GO && doorsOp == CLOSE && currentFloor == $past(currentFloor))[->FLOORS-2])) ##0 (engineOp == STOP && doorsOp == OPEN && currentFloor[FLOORS-1] == 1)[->1];
+    1 ##1 (direction == UP) throughout (##1( engineOp == STOP && doorsOp == OPEN && currentFloor[0] == 1]) ##1 ( engineOp == GO && doorsOp == CLOSE && currentFloor == $past(currentFloor)) (##1( engineOp == STOP && doorsOp == OPEN && currentFloor == $past(currentFloor << 1) ##1 ( engineOp == GO && doorsOp == CLOSE && currentFloor == $past(currentFloor))[->FLOORS-2]));
 endsequence
 
 sequence sQ8;
