de779b0d009ef01a87619a9683f159067f1bf25d
Maayan Abramov <maayan312a@gmail.com>
2025-12-27 20:51:11 +0200

    .

--- assignments/assignment03/reqgnt.sv
+++ assignments/assignment03/reqgnt.sv
@@ -12,9 +12,39 @@ module reqgnt(
 // 4. Do not change the label of the assert (keep it "A").
 
 // IMPLEMENT THE AUXILIARY CODE HERE
+input clk, rst, req, gnt;
+// count number of open reqs
+reg signed [3:0] cnt;
+reg [0:1] A1;
+reg [0:5] A2;
+always @(posedge clk) 
+begin
+    if (rst) 
+        begin
+            cnt <= 4’b0000;
+            A1 <= 2'b00;
+            A2 <= 6'b000000;
+        end 
+    else
+        begin
+            if (req) A1[0] <= 1'b1;
+            if (gnt) begin
+                if (A2[5])      A2[5] <= 1'b0;
+                else if (A2[4]) A2[4] <= 1'b0;
+                else if (A2[3]) A2[3] <= 1'b0;
+                else if (A2[2]) A2[2] <= 1'b0;
+                else if (A2[1]) A2[1] <= 1'b0;
+                else if (A2[0]) A2[0] <= 1'b0;
+            end
+            A2 <= (A2 >> 1);
+            A2[0] <= A1[1];
+            A1 <= (A1 >> 1);
+        end
+end
+
 
 property P;
-    @(posedge clk) (1); // IMPLEMENT THE PROPERTY HERE
+    @(posedge clk) (gnt |-> A2 != 6'b000000)&& (! (!gnt && (A2[5] == 1'b1))); // IMPLEMENT THE PROPERTY HERE
 endproperty
 
 A: assert property (P);
