eab91048674e35adc2eb61d947a4dc5b87610a5f
Ziv Damir <zivdamir@Zivs-MacBook-Air.local>
2025-12-29 11:25:55 +0200

    triying ff

--- assignments/assignment03/fifo.sv
+++ assignments/assignment03/fifo.sv
@@ -79,7 +79,7 @@ module fifo #(
   logic [WIDTH-1:0] value;
   logic is_sampled;
   logic [PTRW:0] items_ahead;
-  always_ff @(posedge clk or negedge rst)
+  always_ff @(posedge clk)
   begin
 	if(~rst)
 	 begin
