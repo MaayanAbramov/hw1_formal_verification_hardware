86056fee6f2f498198cf93a61a2c239a65126941
Ziv Damir <zivdamir@Zivs-MacBook-Air.local>
2025-12-29 12:00:33 +0200

    .

--- assignments/assignment03/reqgnt.sv
+++ assignments/assignment03/reqgnt.sv
@@ -14,32 +14,30 @@ module reqgnt(
 // IMPLEMENT THE AUXILIARY CODE HERE
 
 // count number of open reqs
-reg [0:1] A1;
-reg [0:5] A2;
+ralways @(posedge clk) begin
+    if (rst) begin
+        A1 <= 2'b00;
+        A2 <= 6'b000000;
+    end else begin
+        // ---- A1: 2-cycle request pipeline ----
+        A1[0] <= req;
+        A1[1] <= A1[0];
 
-always @(posedge clk) 
-begin
-    if (rst) 
-        begin
-            A1 <= 2'b00;
-            A2 <= 6'b000000;
-        end 
-    else
-        begin
-            if (req) A1[0] <= 1'b1;
-            if (gnt) begin
-                if (A2[5])      A2[5] <= 1'b0;
-                else if (A2[4]) A2[4] <= 1'b0;
-                else if (A2[3]) A2[3] <= 1'b0;
-                else if (A2[2]) A2[2] <= 1'b0;
-                else if (A2[1]) A2[1] <= 1'b0;
-                else if (A2[0]) A2[0] <= 1'b0;
-            end
-            A2 <= (A2 >> 1);
-            A2[0] <= A1[1];
-            A1 <= (A1 >> 1);
+        // ---- A2: age pending requests ----
+        A2 <= {A2[4:0], A1[1]};
+
+        // ---- Grant removes oldest pending request ----
+        if (gnt) begin
+            if      (A2[5]) A2[5] <= 1'b0;
+            else if (A2[4]) A2[4] <= 1'b0;
+            else if (A2[3]) A2[3] <= 1'b0;
+            else if (A2[2]) A2[2] <= 1'b0;
+            else if (A2[1]) A2[1] <= 1'b0;
+            else if (A2[0]) A2[0] <= 1'b0;
         end
+    end
 end
+
 
 property P;
     @(posedge clk)
